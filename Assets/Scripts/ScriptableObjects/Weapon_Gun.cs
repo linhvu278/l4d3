c093a3c3fdceb9ee767132c50e1c6e8737ca8f47
Linh Vu <vuvietlinh11@gmail.com>
2023-12-30 17:49:23 +0700

    removed unused variables

--- Assets/Scripts/ScriptableObjects/Weapon_Gun.cs
+++ Assets/Scripts/ScriptableObjects/Weapon_Gun.cs
@@ -7,10 +7,11 @@ public class Weapon_Gun : Weapon
     public AmmoType ammoType;
     public FireMode fireMode;
     // public int clipAmmo;
-    public float damage, fireRate, inaccuracy, /*inaccuracyMove,*/ range, reloadTime, maxDurability;
+    public float damage, fireRate, inaccuracy, /*inaccuracyMove,*/ range, reloadTime/*, maxDurability*/;
     public bool slowReload;
     public Vector3 aimPosition, aimSightPosition;
+    // public Item weaponUpgradeType;
 }
-public enum GunType{ rifle, shotgun, smg, sniper, pistol }
-public enum AmmoType{ ammo_762, ammo_556, ammo_12g, ammo_45acp, ammo_9mm, ammo_magnum }
-public enum FireMode{ single, auto, burst}
+public enum GunType { rifle, shotgun, smg, sniper, pistol }
+public enum AmmoType { ammo_762, ammo_556, ammo_12g, ammo_45acp, ammo_9mm, ammo_magnum }
+public enum FireMode { single, auto, burst }
