22d10bde17715749e2a9b4918068f081f80efe2c
Linh Vu <vuvietlinh11@gmail.com>
2024-07-08 20:59:34 +0700

    readjusted UI elements

--- Assets/Scripts/UI/LoadoutUI.cs
+++ Assets/Scripts/UI/LoadoutUI.cs
@@ -1,6 +1,3 @@
-// using System;
-// using System.Collections;
-// using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 using TMPro;
@@ -12,40 +9,61 @@ public class LoadoutUI : MonoBehaviour
     // private Inventory inventory;
     private ItemDatabase db;
 
-    Transform loadoutUI;
+    // Transform loadoutUI;
     Image icon, ammoIcon;
-    TextMeshProUGUI ammo, ammoReserve;
+    TextMeshProUGUI ammo_main, ammo_side, ammoReserve_main, ammoReserve_side;
     Color white, green;
 
+    #region get hud elements
+
     public void GetHUDIcon(Sprite weaponIcon, int weaponIndex){
-        icon = loadoutUI.GetChild(weaponIndex).Find("Icon").GetComponent<Image>();
+        icon = transform.GetChild(weaponIndex).Find("Icon").GetComponent<Image>();
         icon.sprite = weaponIcon;
         icon.enabled = true;
     }
 
-    public void GetHUDAmmo(int gunAmmo, int weaponIndex){
-        ammo = loadoutUI.GetChild(weaponIndex).Find("Ammo").GetComponent<TextMeshProUGUI>();
-        ammo.text = gunAmmo.ToString();
-        ammo.enabled = true;
+    public void GetHUDAmmo(int ammo, int weaponIndex){
+        switch (weaponIndex){
+            case (int)WeaponCategory.primary:
+                ammo_main.enabled = true;
+                ammo_main.text = ammo.ToString();
+                break;
+            case (int)WeaponCategory.secondary:
+                ammo_side.enabled = true;
+                ammo_side.text = ammo.ToString();
+                break;
+            default: break;
+        }
     }
 
-    public void GetHUDMaxAmmo(int gunMaxAmmo, int weaponIndex){
-        ammoReserve = loadoutUI.GetChild(weaponIndex).Find("AmmoReserve").GetComponent<TextMeshProUGUI>();
-        ammoReserve.text = gunMaxAmmo.ToString();
-        ammoReserve.enabled = gunMaxAmmo > 0;
+    public void GetHUDMaxAmmo(int maxAmmo, int weaponIndex){
+        switch (weaponIndex){
+            case (int)WeaponCategory.primary:
+                ammoReserve_main.enabled = maxAmmo > 0;
+                ammoReserve_main.text = maxAmmo.ToString();
+                break;
+            case (int)WeaponCategory.secondary:
+                ammoReserve_side.enabled = maxAmmo > 0;
+                ammoReserve_side.text = maxAmmo.ToString();
+                break;
+            default: break;
+        }
     }
 
     public void GetHUDAmmoIcon(ItemType gunAmmoType, int weaponIndex){
-        ammoIcon = loadoutUI.GetChild(weaponIndex).Find("AmmoIcon").GetComponent<Image>();
+        ammoIcon = transform.GetChild(weaponIndex).Find("AmmoIcon").GetComponent<Image>();
         ammoIcon.sprite = db.GetItemByType(gunAmmoType).itemIcon;
         ammoIcon.enabled = true;
     }
 
+    #endregion
+
+    #region update hud elements
+    
     // change HUD color of currently selected weapon
-    public void ChangeHUDColor(int selectedWeapon)
-    {
-        // change color of the outer frames to white
-        foreach (Transform loadoutItem in loadoutUI) {
+    public void ChangeHUDColor(int selectedWeapon){
+        // change color of outer frames to white
+        foreach (Transform loadoutItem in transform) {
             if (loadoutItem.TryGetComponent(out Image frame)) frame.color = white;
             
             // change color of all other items in the loadout to white
@@ -55,22 +73,24 @@ public class LoadoutUI : MonoBehaviour
             }
         }
         
-        // change color of selected item's frame to green
-        loadoutUI.GetChild(selectedWeapon).GetComponent<Image>().color = green;
+        // change color of selected item's outer frame to green
+        transform.GetChild(selectedWeapon).GetComponent<Image>().color = green;
 
-        // change the color of selected item to green
-        foreach (Transform child in loadoutUI.GetChild(selectedWeapon)) {
+        // change the color of selected item's inner icons to green
+        foreach (Transform child in transform.GetChild(selectedWeapon)) {
             if (child.TryGetComponent(out Image image)) image.color = green;
             if (child.TryGetComponent(out TextMeshProUGUI tmpro)) tmpro.color = green;
         }
     }
 
     public void RemoveHUDElements(int weaponIndex){
-        foreach (Transform child in loadoutUI.GetChild(weaponIndex)){
+        foreach (Transform child in transform.GetChild(weaponIndex)){
             if (child.TryGetComponent(out Image image)) image.enabled = false;
             if (child.TryGetComponent(out TextMeshProUGUI tmpro)) tmpro.enabled = false;
         }
     }
+
+    #endregion
 
     void Awake()
     {
@@ -85,6 +105,7 @@ public class LoadoutUI : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        // loadoutUI = transform;
         // inventory = Inventory.instance;
         // db = ItemDatabase.instance;
         db = GameObject.FindGameObjectWithTag("GameController").GetComponent<ItemDatabase>();
@@ -94,8 +115,7 @@ public class LoadoutUI : MonoBehaviour
         green = Color.green;
         green.a = .75f;
 
-        loadoutUI = transform;
-        foreach (Transform loadoutItem in loadoutUI){
+        foreach (Transform loadoutItem in transform){
             loadoutItem.GetComponent<Image>().color = white;
             foreach (Transform child in loadoutItem){
                 if (child.TryGetComponent(out Image image)){
@@ -108,5 +128,10 @@ public class LoadoutUI : MonoBehaviour
                 }
             }
         }
+
+        ammo_main = transform.GetChild((int)WeaponCategory.primary).Find("Ammo").GetComponent<TextMeshProUGUI>();
+        ammo_side = transform.GetChild((int)WeaponCategory.secondary).Find("Ammo").GetComponent<TextMeshProUGUI>();
+        ammoReserve_main = transform.GetChild((int)WeaponCategory.primary).Find("AmmoReserve").GetComponent<TextMeshProUGUI>();
+        ammoReserve_side = transform.GetChild((int)WeaponCategory.secondary).Find("AmmoReserve").GetComponent<TextMeshProUGUI>();
     }
 }
